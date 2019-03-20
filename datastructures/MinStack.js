3f02f86bcf930af07701a7378948c94d7e228781
i859464 <vyshnavi.konda@sap.com>
2019-03-19 21:56:26 -0700

    Adding DS for MinStack

---
+++ datastructures/MinStack.js
@@ -0,0 +1,62 @@
+/**
+* Initialize a stack row
+*/
+var StackRow = function(){
+    this.value = null;
+    this.min = null;
+};
+
+/**
+ * initialize your data structure here.
+ */
+
+var MinStack = function() {
+    this.stack = [];
+    this.stackMin;
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MinStack.prototype.push = function(x) {
+    
+    let stackRow = new StackRow();
+    stackRow.value = x;
+    stackRow.min = this.stackMin; //Stack row contains current min in the stack
+    this.stack.push(stackRow);
+    
+    //Updating the minimum value based on new.
+    if(this.stack.length === 1){
+        this.stackMin = x;
+    }
+    
+    if(x < this.stackMin){
+        this.stackMin = x;
+    }
+    
+    
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    let popped = this.stack.pop();
+    this.stackMin = popped.min;
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    let top = this.stack[this.stack.length - 1];
+    return top.value;    
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.stackMin;
+};
